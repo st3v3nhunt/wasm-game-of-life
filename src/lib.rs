9bab89b5d36ed358baa01552abe53490c446060d
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-01-02 18:48:11 +0000

    feat: add timers for key events

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ extern crate js_sys;
 
 use fixedbitset::FixedBitSet;
 use std::fmt;
+use utils::Timer;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -41,33 +42,34 @@ impl Universe {
     }
 
     pub fn tick(&mut self) {
-        let mut next = self.cells.clone();
+        let _timer = Timer::new("Universe::tick");
+        let mut next = {
+            let _timer = Timer::new("allocate next cells");
+            self.cells.clone()
+        };
 
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let idx = self.get_index(row, col);
-                let cell = self.cells[idx];
-                let live_neighbors = self.live_neighbor_count(row, col);
+        {
+            let _timer = Timer::new("new generation");
+            for row in 0..self.height {
+                for col in 0..self.width {
+                    let idx = self.get_index(row, col);
+                    let cell = self.cells[idx];
+                    let live_neighbors = self.live_neighbor_count(row, col);
 
-                // log!(
-                //     "cell[{}, {}] is initially {:?} and has {} live neighbors",
-                //     row,
-                //     col,
-                //     cell,
-                //     live_neighbors
-                // );
-                next.set(
-                    idx,
-                    match (cell, live_neighbors) {
-                        (true, x) if x < 2 => false,
-                        (true, 2) | (true, 3) => true,
-                        (true, x) if x > 3 => false,
-                        (false, 3) => true,
-                        (otherwise, _) => otherwise,
-                    },
-                );
+                    next.set(
+                        idx,
+                        match (cell, live_neighbors) {
+                            (true, x) if x < 2 => false,
+                            (true, 2) | (true, 3) => true,
+                            (true, x) if x > 3 => false,
+                            (false, 3) => true,
+                            (otherwise, _) => otherwise,
+                        },
+                    );
+                }
             }
         }
+        let _timer = Timer::new("free old cells");
         self.cells = next;
     }
 
